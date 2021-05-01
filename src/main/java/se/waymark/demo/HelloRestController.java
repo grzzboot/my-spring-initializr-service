074c2bd1f5e7cf3d66046206334bf0837d49d8fe
andreas.engstrom <andreas.engstrom@taxistockholm.se>
2021-05-01 16:57:31 +0200

    Super simple demo service

---
+++ src/main/java/se/waymark/demo/HelloRestController.java
@@ -0,0 +1,16 @@
+package se.waymark.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloRestController {
+
+	@GetMapping
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("Hello");
+	}
+
+}
